1bb5d1a08780ec032ce04712893844ef65962f48
alpin1234567 <74572180+alpin1234567@users.noreply.github.com>
2021-03-13 19:25:34 +0700

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Free REST APIs</title>
+
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
+
+<link type="text/css" rel="stylesheet" href="https://colorlib.com/etc/404/colorlib-error-404-8/css/style.css" />
+
+
+<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+</head>
+<body>
+<div id="notfound">
+<div class="notfound">
+<div class="notfound-404">
+<h1>A<span>p</span>i</h1>
+</div>
+<p>Documentation</p>
+<p>Creator: alpinofc404</p>
+<a href="https://apikeyalpinofc.herokuapp.com">home page</a>
+<br><br>
+<font color="#b3b3b3">Api Status :</font><font color="#00cc00"> Online</font>
+
+</div>
+</div>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+</body>
+</html>
